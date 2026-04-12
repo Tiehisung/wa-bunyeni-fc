07f1b7b6b18c22de525c15d239385528246bdd50
Alhassan Ibrahim <tiehisungai@gmail.com>
2026-04-12 07:15:56 +0000

    cleaning

--- data/team.ts
+++ data/team.ts
@@ -2,15 +2,15 @@ import { ENV } from "@/lib/env";
 import { ITeam } from "@/types/match.interface";
 
 export const TEAM: ITeam & { url: string, officialSignature: string } = {
-    _id: ENV.TEAM_ID,// Example ObjectId
-    url: ENV.APP_URL,
-    name: ENV.TEAM_NAME,
-    alias: ENV.TEAM_ALIAS,
-    community: "Wa-Konjiehi",
-    logo: ENV.LOGO_NO_BG_URL,
-    officialSignature: ENV.OFFICIAL_SIGNATURE_URL,
+    _id: ENV.TEAM_ID as string,
+    url: ENV.APP_URL as string,
+    name: ENV.TEAM_NAME as string,
+    alias: ENV.TEAM_ALIAS as string,
+    community: "Wa-Konjiehi" as string,
+    logo: ENV.LOGO_NO_BG_URL as string,
+    officialSignature: ENV.OFFICIAL_SIGNATURE_URL as string,
     currentPlayers: [],
-    contact: ENV.CONTACT.PHONE,
+    contact: ENV.CONTACT.PHONE as string,
     contactName: "Adam Wahid",
     createdAt: "2023-11-28T10:30:00Z",
     updatedAt: "2023-11-28T10:30:00Z",
