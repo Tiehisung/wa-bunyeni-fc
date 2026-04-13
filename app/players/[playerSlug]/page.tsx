b24f3dd218e944627cadf905f9e81cb379086111
Alhassan Ibrahim <tiehisungai@gmail.com>
2026-04-13 20:09:46 +0000

    fix types of finance

--- app/players/[playerSlug]/page.tsx
+++ app/players/[playerSlug]/page.tsx
@@ -9,43 +9,27 @@ import {
   useGetPlayersQuery,
   useGetPlayerStatsQuery,
 } from "@/services/player.endpoints";
-import PageLoader from "@/components/loaders/Page";
-import DataErrorAlert from "@/components/error/DataError";
 
-import useGetParam from "@/hooks/params";
 import { useParams } from "next/navigation";
 
 export default function PlayerProfilePage() {
   const playerSlug = useParams().playerSlug;
 
-  const { data: playersData, isLoading: playersLoading } =
-    useGetPlayersQuery("");
+  const { data: playersData } = useGetPlayersQuery("");
 
-  const { data: galleriesData, isLoading: galleriesLoading } =
-    useGetGalleriesQuery(`tags=${playerSlug}`);
-  const {
-    data: statsData,
-    isLoading: statsLoading,
-    error,
-  } = useGetPlayerStatsQuery(playerSlug || "");
+  const { data: galleriesData } = useGetGalleriesQuery(`tags=${playerSlug}`);
+  const { data: statsData } = useGetPlayerStatsQuery(
+    playerSlug?.toString() || "",
+  );
 
-  const isLoading = playersLoading || galleriesLoading || statsLoading;
   const players = playersData;
   const galleries = galleriesData;
   const playerStats = statsData;
 
-  const player = players?.data?.find((p) => p._id === playerSlug);
-
-
-
   return (
     <>
       <main className="pl-2">
-        <PlayerProfile
-           
-          galleries={galleries?.data}
-          stats={playerStats?.data}
-        />
+        <PlayerProfile galleries={galleries?.data} stats={playerStats?.data} />
         <PlayerHeadList players={players?.data as IPlayer[]} />
       </main>
     </>
