4bb5e319ebd8c400ecf99b602c95f3cdbeb503d4
Alhassan Ibrahim <tiehisungai@gmail.com>
2026-04-24 08:38:13 +0000

    match card

--- app/matches/(cards)/Modern.tsx
+++ app/matches/(cards)/Modern.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
-import { checkTeams } from "@/lib/compute/match";
+import { checkMatchMetrics, checkTeams } from "@/lib/compute/match";
 import { formatDate } from "@/lib/timeAndDate";
 import { IMatch } from "@/types/match.interface";
 import Link from "next/link";
@@ -10,7 +10,9 @@ interface Props {
   match?: IMatch;
 }
 const ModernFixtureCard = ({ match }: Props) => {
-  const { home, away, } = checkTeams(match);
+  const { home, away } = checkTeams(match);
+  const { goals, winStatus } = checkMatchMetrics(match);
+
   return (
     <div className="bg-card rounded-xl p-5 flex flex-wrap items-center justify-between gap-4 hover:shadow-lg transition-shadow border border-muted">
       <div className="flex items-center gap-4 w-full md:w-auto ">
@@ -34,12 +36,8 @@ const ModernFixtureCard = ({ match }: Props) => {
             variant={"secondary"}
             className=" px-4 py-1 rounded-full text-sm font-bold"
           >
-            {match?.computed.teamScore} - {match?.computed.opponentScore}{" "}
-            {match?.computed?.result === "win"
-              ? "🏆"
-              : match?.computed?.result === "loss"
-                ? "😞"
-                : "🤝"}
+            {goals?.home} - {goals?.away}{" "}
+            {winStatus === "win" ? "🏆" : winStatus === "loss" ? "😞" : "🤝"}
           </Badge>
         ) : (
           <Badge
@@ -50,14 +48,10 @@ const ModernFixtureCard = ({ match }: Props) => {
           </Badge>
         )}
         <Link
-          href={
-            match?.computed?.result
-              ? `/highlights?tag=${match?._id}`
-              : "/donation"
-          }
+          href={winStatus ? `/highlights?tag=${match?._id}` : "/donation"}
           className="text-primary hover:text-secondary text-sm font-medium transition-colors"
         >
-          {match?.computed?.result ? "📺 Highlights →" : "🎫 Support Match →"}
+          {winStatus ? "📺 Highlights →" : "🎫 Support Match →"}
         </Link>
       </div>
     </div>
