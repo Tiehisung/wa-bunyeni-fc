edb17945c8f9be60eebc9a95c74933da36762c87
Alhassan Ibrahim <tiehisungai@gmail.com>
2026-04-29 15:20:25 +0000

    news

--- app/admin/news/[slug]/ClientItem.tsx
+++ app/admin/news/[slug]/ClientItem.tsx
@@ -5,13 +5,11 @@ import FileRenderer from "@/components/files/FileRender";
 import { IFileProps } from "@/types/file.interface";
 import { ConfirmActionButton } from "@/components/buttons/ConfirmAction";
 import { shortText } from "@/lib";
-
 import { INewsProps } from "@/types/news.interface";
 import {
   useDeleteNewsMutation,
   useUpdateNewsMutation,
 } from "@/services/news.endpoints";
-
 import { smartToast } from "@/utils/toast";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
