34b9fe437debd2b5f077f62089572880d1d3333c
tiehisung <tiehisungai@gmail.com>
2026-05-07 23:55:47 +0000

    get players params

--- app/players/new-signing/page.tsx
+++
@@ -1,9 +0,0 @@
-import PlayerProfileForm from "@/app/admin/players/new/NewSigningForms";
-
-export default function NewPlayerSigninPage() {
-  return (
-    <div>
-      <PlayerProfileForm />
-    </div>
-  );
-}
